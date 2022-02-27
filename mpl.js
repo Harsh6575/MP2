001f1a6b8c95843aea7ca7990d4515cc46f8c6d1
Harsh6575 <95074155+Harsh6575@users.noreply.github.com>
2022-02-27 09:51:36 +0530

    first commit

---
+++ mpl.js
@@ -0,0 +1,14 @@
+<label id="Greetings"></label>
+<script>
+const time = new Date().getHours;
+let Greet;
+if (time < 12) {
+Greet = "Good Morning";
+}
+else if (time > 12 && time < 20) {
+greeting = "Good Afternoon";
+}
+else {
+greeting = "Good Evening";
+}
+</script>
